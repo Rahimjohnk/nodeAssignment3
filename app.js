6ca9c4935eff4ae22297ec75aa4ca7c82cbeb716
Rahim <k.rahim001@gmail.com>
2023-08-19 19:45:38 +0530

    RJ(static_folder_external_style): sheet added

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 // npm init
 // npm i nodemon --save-dev
 // npm i --seve express
+const path = require('path');
 
 const express = require('express');
 const app = express();
 
-const path = require('path');
-
 const rootDir = require('./util/path.js');
+
+app.use(express.static(path.join(__dirname, 'public')));
 // http://localhost:3000/users
 app.get('/users',(req, res, next)=>{
     res.sendFile(path.join(__dirname, 'view', 'user.html'));
 });
 // http://localhost:3000
-app.use('/', (req, res, next)=>{
+app.get('/', (req, res, next)=>{
     res.sendFile(path.join(rootDir, 'view', 'base.html'));
 });
 
